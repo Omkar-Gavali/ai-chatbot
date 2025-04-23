61e56eb630e4098ced43f219dc6ddd127f0c1036
omkar <Omkar.Gavali@Student.HTW-Berlin.de>
2025-04-23 14:48:28 +0200

    Creaated document.ysx

---
+++ frontend/src/app/pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* Tailwind via CDN */}
+        <script src="https://cdn.tailwindcss.com"></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
